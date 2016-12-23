b859e24818271bf6f620d2442e9488c62349566f
mits9 <mits9@users.noreply.github.com>
2016-12-23 17:42:47 -0600

    First passing test on get_api_version in public

--- pyNiceHashClient/public.py
+++ pyNiceHashClient/public.py
@@ -1,2 +1,12 @@
+import requests
+
+
+# import json
+
 def get_library_version():
     return 0.1
+
+
+def get_api_version():
+    response = requests.get("https://www.nicehash.com/api").json()
+    return response['result']['api_version']
--- tests/test_public.py
+++ tests/test_public.py
@@ -4,8 +4,14 @@ from pyNiceHashClient import public as pub
 
 
 class PublicTestSuite(unittest.TestCase):
+    def setUp(self):
+        pass
+
     def test_get_library_version(self):
         self.assertEquals(pub.get_library_version(), 0.1)
+
+    def test_get_api_version(self):
+        self.assertEquals(pub.get_api_version(), u'1.2.6')
 
 
 if __name__ == '__main__':

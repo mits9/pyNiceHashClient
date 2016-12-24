ff4e8757a3f9410d4c51a8a046f451f5c85d4397
mits9 <mits9@users.noreply.github.com>
2016-12-23 18:15:13 -0600

    Added get_stats_global_current to public

--- tests/test_public.py
+++ tests/test_public.py
@@ -10,6 +10,14 @@ class PublicTestSuite(unittest.TestCase):
     def test_get_api_version(self):
         self.assertEquals(pub.get_api_version(), '1.2.6')
 
+    def test_get_stats_global_current_no_location(self):
+        stats = pub.get_stats_global_current()
+        self.assertTrue(len(stats) == 25)
+
+    def test_get_stats_global_current_location_set(self):
+        stats = pub.get_stats_global_current(0)
+        self.assertTrue(len(stats) == 25)
+
 
 if __name__ == '__main__':
     unittest.main()
